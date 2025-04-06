929e05e2c11d53f37ab367fff712fd7d727aecce
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2025-04-06 14:58:54 -0300

    feat: add db.json and services api to interage of aplication

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,8 +1,40 @@
-
 const Cart = () => {
   return (
-    <div>Carrinho de compras</div>
-  )
-}
+    <div className="w-full max-w-7xl mx-auto ">
+      
+   <h1 className="font-medium text-2xl text-center my-4">Meu Carrinho de compras</h1>
 
-export default Cart
+   <section className="flex items-center justify-between border-b-2 border-gray-300">
+
+   <img    src="https://cdn.awsli.com.br/600x1000/494/494820/produto/439209597de595cc73.jpg" alt="logo do produto" 
+   className="w-28"/>
+
+   <strong>Preço: R$ 1000</strong>
+
+<div className="flex items-center justify-center gap-3">
+  <button className="bg-slate-600  px-2 rounded text-white font-medium flex items-center justify-center">
+    -
+  </button>
+  <button className="bg-slate-600  px-2 rounded text-white font-medium flex items-center justify-center">
+    2
+  </button>
+  <button className="bg-slate-600  px-2 rounded text-white font-medium flex items-center justify-center">
+    +
+  </button>
+</div>
+
+<strong className="float-right">
+SubTotal: R$ 2000
+</strong>
+
+   </section>
+
+   <p className="font-bold mt-4">
+    Total: R$: 1000
+   </p>
+      
+      </div>
+  );
+};
+
+export default Cart;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,53 @@
 import { BsCartPlus } from "react-icons/bs";
+import { useState, useEffect } from "react";
+
+import { api } from "../../services/api";
+
+
+interface ProductProps{
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
 
 const Home = () => {
+const [products, setProducts] = useState<ProductProps[]>([]);
+
+useEffect(()=>{
+async function getProducts() {
+  const response = await api.get("/products")
+  setProducts(response.data)
+}
+getProducts()
+}, [])
+
   return (
     <main className="w-full max-w-7xl px-4 mx-auto ">
       <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
-        {" "}
         Produtos em alta
       </h1>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-        <section className="w-full">
-          <img
-          className="w-full rounded-lg max-h-70 mb-2"
-            src="https://cdn.awsli.com.br/600x1000/494/494820/produto/439209597de595cc73.jpg"
-            alt="logo do produto"
-          />
-          <p className="font-medium mt-1 mb-2 ">Airpods PRO</p>
-          <div className="flex gap-3 items-center">
-            <strong className="text-zinc-700/90">R$ 1000</strong>
-            <button className="bg-zinc-900 p-1 rounded">
-              <BsCartPlus size={24} color="#fff" />
-            </button>
-          </div>
-        </section>
+       {products.map((product)=> (
+         <section key={product.id} className="w-full">
+         <img
+           className="w-full rounded-lg max-h-70 mb-2"
+           src={product.cover}
+           alt={product.title}
+         />
+         <p className="font-medium mt-1 mb-2 ">{product.title}</p>
+         <div className="flex gap-3 items-center">
+           <strong className="text-zinc-700/90">{product.price.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+           })}</strong>
+           <button className="bg-zinc-900 p-1 rounded">
+             <BsCartPlus size={24} color="#fff" />
+           </button>
+         </div>
+       </section>
+       ))}
       </div>
     </main>
   );
